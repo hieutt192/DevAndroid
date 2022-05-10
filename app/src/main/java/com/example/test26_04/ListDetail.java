f1b846104f0a11a07eb518c6976cc69acff7c4f1
nanh <maxstep19@gmail.com>
2022-05-10 08:27:57 +0700

    update qldh

---
+++ app/src/main/java/com/example/test26_04/ListDetail.java
@@ -0,0 +1,5 @@
+package com.example.qldh3;
+
+public class ListDetail {
+
+}
---
+++ app/src/main/java/com/example/test26_04/list_item_index.java
@@ -0,0 +1,14 @@
+package com.example.qldh3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_item_index extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item_index);
+    }
+}
