4d3fa2afba61b13acc5ec50b6be1374b175b8655
hieutt192 <hieutt192@gmail.com>
2022-05-11 09:38:17 +0700

    update product

--- app/src/main/java/com/example/test26_04/ProductActivity.java
+++ app/src/main/java/com/example/test26_04/ProductActivity.java
@@ -1,6 +1,8 @@
 package com.example.test26_04;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.test26_04.utils.ProductAdapter;
 import com.example.test26_04.R;
 import com.example.test26_04.models.Product;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 import java.util.ArrayList;
@@ -22,6 +25,7 @@ public class ProductActivity extends AppCompatActivity {
 
     private RecyclerView rcvData;
     private ProductAdapter productAdapter;
+    private FloatingActionButton btnAddP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,15 @@ public class ProductActivity extends AppCompatActivity {
 
         productAdapter = new ProductAdapter(this,getListProduct());
         rcvData.setAdapter(productAdapter);
+
+        btnAddP = findViewById(R.id.btnAddnew);
+        btnAddP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Add_ProductActivity.class));
+            }
+        });
+
     }
 
     private List<Product> getListProduct() {
